4e442994a7b450c27b4e77c28d9ea2c4d4fcddfd
FernandoVasconcelos <fernando.vasconcelos@cenergel.com.br>
2021-05-04 09:12:56 -0300

    Guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,18 +15,20 @@ fn main()
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-                Err(_) => continue,
+            Err(_) => continue,
         };
+
         match guess.cmp(&secret_number)
         {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-            println!("You win!");
-            println!("The secret number is: {}", secret_number);
-            break;
+                println!("You win!");
+                println!("The secret number is: {}", secret_number);
+                break;
             }
         }
         println!("You guessed: {}", guess);
