efba336b3311ff97a1bf3a4cbe6fb5ea379c6674
FernandoVasconcelos <fernando.vasconcelos@cenergel.com.br>
2021-05-04 11:08:14 -0300

    Loops

--- funcs/src/main.rs
+++ funcs/src/main.rs
@@ -3,6 +3,16 @@ fn main()
     println!("Hello, world!");
     let x = another_function(5, 6);
     println!("The value of x is: {}", x);
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter()
+    {
+        println!("The value is: {}", element);
+    }
+    for number in (1..4).rev()
+    {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
 
 fn another_function(x: i32, y: i32) -> i32
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,7 @@ fn main()
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    loop
+    while true
     {
         println!("Please input your guess.");
         let mut guess = String::new();
